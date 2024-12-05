e1d6deb6fac5f98b1926f2ac45975fa42a8650a6
enow john enowbi <johnwashinton88@gmail.com>
2024-12-05 17:23:00 +0100

    initializing

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+
+function rollDice(){
+    const numofdice = document.getElementById("numofdice").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImage = document.getElementById("diceImage");
+
+    const values = [];
+    const images = [];
+
+    for(let i = 0; i < numofdice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src = "images/${value}.jpg">`);
+    }
+
+    diceResult.textContent = `dice: ${values.join(", ")}`
+    diceImage.innerHTML = images.join(" ");
+}
